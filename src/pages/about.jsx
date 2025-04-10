78e17eff25cd5bc77a61b04f1c203cbfa143013f
Roshami <grtmybusiness@gmail.com>
2025-04-10 22:36:01 +0530

    secure first

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ export default function About() {
     return (
         <div>
             <h1>About</h1>
-            <img src="/profile/dp.jpeg" alt="about me" className="w-1/2 rounded-[50%] " />
+            <img src="/profile/dp.jpeg" alt="about me" className="w-1/2 rounded-[50%] select-none" />
 
 
         </div>
